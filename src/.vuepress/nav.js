7b022308b9617fd22cd09cd4d309793ec7c064bb
zhoubichuan <zhoubichuan@icloud.com>
2024-05-18 20:08:19 +0800

    更新文档

---
+++ src/.vuepress/nav.js
@@ -0,0 +1,24 @@
+const nav = [{
+  text: '开发工具',
+  items: [
+    {
+      text: '1.svn基础',
+      link: '/base/build/1.index'
+    },
+    {
+      text: '2.git基础',
+      link: '/base/practice/1.Tools',
+    }
+  ]
+},
+{
+  text: '开发软件',
+  items: [
+    {
+      text: '1.gitlab',
+      link: '/senior/use/1.recursionAndDynamics'
+    },
+  ]
+}
+];
+module.exports = nav;
---
+++ src/.vuepress/sidebar.js
@@ -0,0 +1,41 @@
+const sidebar = {
+  '/base/build/': [
+    '1.index',
+    '2.service',
+    '3.cdn',
+    '4.cors',
+    '5.cache',
+    '7.module',
+    '8.project',
+    '9.utils',
+    '10.ui',
+    '11.data',
+    '12.skill',
+    '13.com',
+    '14.data',
+    '15.api'
+  ],
+  '/base/practice/': [
+    '1.Tools',
+    '2.Jenkins',
+    '3.gitlab',
+    'vscode',
+    'test',
+    'prem',
+    'i18n'
+  ],
+  '/senior/use/': [
+    '1.recursionAndDynamics',
+    '2.extend',
+    '3.componentCommunication',
+    '4.render',
+    '5.api',
+    '6.form',
+    '7.checkBox',
+    '8.active',
+    '9.alert',
+    '10.table',
+    '11.tree'
+  ],
+};
+module.exports = sidebar;
